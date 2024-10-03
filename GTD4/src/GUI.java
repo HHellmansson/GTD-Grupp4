dee41ffe391540b0b53b62d709b1c68f88308dcb
HHellmansson <hugoheggs@gmail.com>
2024-10-03 13:16:15 +0200

    ta bort knapp
    fixade en ta bort knapp och ändrade lite saker

--- GTD4/src/GUI.java
+++ GTD4/src/GUI.java
@@ -1,68 +1,108 @@
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 
 public class GUI {
 
     int countingClicks = 0; // Håller koll på hur många gånger man tryckt på Lägga till-knappen
 
-    ArrayList<Integer> textFieldArray = new ArrayList<Integer>();
-    ArrayList<Integer> taBort = new ArrayList<Integer>();             //Bättre att göra listor med objekt där man kan spara textfield, checkbox och knappar istället för bara ints?
-    ArrayList<Integer> checkbox = new ArrayList<Integer>();
+    // Lista för att hålla alla rader med komponenter
+    ArrayList<ToDoItem> toDoItems = new ArrayList<>();
 
     JLabel label = new JLabel("Att göra-lista");
-    JTextField textField = new JTextField("");
-    JCheckBox check = new JCheckBox(); 
-    JButton taBortKnapp = new JButton("Ta bort");
     JButton läggTillKnapp = new JButton("Lägg till");
     JPanel startPanel = new JPanel();
     JPanel toDoPanel = new JPanel();
 
-    public GUI(){
+    public GUI() {
 
         JFrame frame = new JFrame("Mitt första GUI");
-        frame.setSize(400, 300);
+        frame.setSize(600, 300);
 
-        läggTillKnapp.addActionListener(e -> {
-            addToDo(countingClicks);
-            // toDoPanel.setLayout(new GridLayout(countingClicks + 1, 3));     Använda?
-            frame.add(toDoPanel);
+        // Använd GridLayout för att hantera rader med komponenter
+        //toDoPanel.setLayout(new FlowLayout()); // Dynamiskt antal rader, 3 kolumner (Textfält, Checkbox, Ta bort-knapp)
+
+        läggTillKnapp.addActionListener(new ActionListener() {
+            @Override public void actionPerformed(ActionEvent e){
+                addToDo();
+            }
         });
 
         startPanel.add(label);
         startPanel.add(läggTillKnapp);
+        startPanel.setLayout(new BoxLayout(startPanel, BoxLayout.Y_AXIS));
 
-        frame.add(startPanel);
+
+        frame.add(startPanel, "North");
+        frame.add(toDoPanel, "Center");
+
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    
+    // Metod som lägger till en ny rad med textfält, checkbox och ta bort-knapp
+    private void addToDo() {
+        countingClicks++;
+
+        // Skapa nya komponenter för raden
+        JPanel ToDo = new JPanel();
+        ToDo.setLayout(new FlowLayout());
+        JTextField nrClicks = new JTextField(25);
+        ToDo.add(nrClicks);
+        JCheckBox checkBox = new JCheckBox();
+        ToDo.add(checkBox);
+        JButton removeButton = new JButton("Ta bort");
+        removeButton.addActionListener(new ActionListener() {
+            @Override public void actionPerformed(ActionEvent e){
+                startPanel.remove(ToDo);
+                startPanel.revalidate();
+                startPanel.repaint();
+            }
+        });
+        ToDo.add(removeButton);
+
+        startPanel.add(ToDo);
+        startPanel.revalidate();
+
+
+        // Skapa och lagra en ny rad (ToDoItem)
+        //ToDoItem newItem = new ToDoItem(nrClicks, checkBox);
+        //toDoItems.add(newItem);
+
+        // Lägg till komponenterna i panelen
+        //toDoPanel.add(nrClicks);
+        //toDoPanel.add(checkBox);
+        //toDoPanel.add(removeButton);
+
+        // Lyssnare för "Ta bort"-knappen, tar bort raden från både GUI och listan
+        
+    }
+
+    // Metod som tar bort en rad från både panelen och listan
+   
+    // Klass för att representera en rad med komponenter
+    class ToDoItem {
+        JTextField textField;
+        JCheckBox checkBox;
+        
+
+        ToDoItem(JTextField textField, JCheckBox checkBox) {
+            this.textField = textField;
+            this.checkBox = checkBox;
+            
+        }
+    }
+
     public static void main(String[] args) {
-        new GUI();
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override public void run(){
+                new GUI();
+            }
+        });
     }
-
-
-    // // Metod som lyssnar på Lägga till-knapppen och skapar en ny rad för text, checkbox och Tabort-knapp.
-    public int addToDo(int antalKlick) {
-            countingClicks++;
-            JTextField nrClicks = new JTextField(25); 
-            JCheckBox checkB = new JCheckBox();                  
-            JButton butt = new JButton("Ta bort");         
-            toDoPanel.add(nrClicks);
-            toDoPanel.add(checkB);
-            toDoPanel.add(butt);
-            textFieldArray.add(countingClicks);
-            checkbox.add(countingClicks);
-            taBort.add(countingClicks);    
-            return countingClicks;
-    }
-
 }
-
-//method för ta bort knappeen så den tar bort rutan.
