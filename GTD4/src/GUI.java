18590467135928e24cf41f9bbed88b70248dd4ee
Rebecca Liesén <rebecca.liesen@hotmail.com>
2024-10-03 22:02:09 +0200

    Tog bort lite bös och gjorde lite mer ordning

--- GTD4/src/GUI.java
+++ GTD4/src/GUI.java
@@ -1,32 +1,25 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.BorderLayout;
 import java.awt.FlowLayout;
-import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.util.ArrayList;
+
 
 public class GUI {
 
-    int countingClicks = 0; // Håller koll på hur många gånger man tryckt på Lägga till-knappen
-
-    // Lista för att hålla alla rader med komponenter
-    ArrayList<ToDoItem> toDoItems = new ArrayList<>();
-
-    JLabel label = new JLabel("Att göra-lista");
+    JLabel label = new JLabel("");
     JButton läggTillKnapp = new JButton("Lägg till");
     JPanel startPanel = new JPanel();
     JPanel toDoPanel = new JPanel();
 
+
+
+    //Metod som skapar ram för programmet och lägger till startpanel, etc.
     public GUI() {
 
-        JFrame frame = new JFrame("Mitt första GUI");
-        frame.setSize(600, 300);
+        JFrame frame = new JFrame("Att göra-lista");
+        frame.setSize(400, 300);
 
-        // Använd GridLayout för att hantera rader med komponenter
-        //toDoPanel.setLayout(new FlowLayout()); // Dynamiskt antal rader, 3 kolumner (Textfält, Checkbox, Ta bort-knapp)
-
+        //Lyssnar på Lägg till-knappen och kallar på addToDo-metoden
         läggTillKnapp.addActionListener(new ActionListener() {
             @Override public void actionPerformed(ActionEvent e){
                 addToDo();
@@ -37,7 +30,6 @@ public class GUI {
         startPanel.add(läggTillKnapp);
         startPanel.setLayout(new BoxLayout(startPanel, BoxLayout.Y_AXIS));
 
-
         frame.add(startPanel, "North");
         frame.add(toDoPanel, "Center");
 
@@ -45,18 +37,21 @@ public class GUI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
+
+
     // Metod som lägger till en ny rad med textfält, checkbox och ta bort-knapp
     private void addToDo() {
-        countingClicks++;
 
         // Skapa nya komponenter för raden
         JPanel ToDo = new JPanel();
         ToDo.setLayout(new FlowLayout());
-        JTextField nrClicks = new JTextField(25);
-        ToDo.add(nrClicks);
+        JTextField textField = new JTextField(25);
+        ToDo.add(textField);
         JCheckBox checkBox = new JCheckBox();
         ToDo.add(checkBox);
         JButton removeButton = new JButton("Ta bort");
+        
+        //Lyssnar på Ta bort-knappen och raderar textfält, checkbox och knapp
         removeButton.addActionListener(new ActionListener() {
             @Override public void actionPerformed(ActionEvent e){
                 startPanel.remove(ToDo);
@@ -65,38 +60,24 @@ public class GUI {
             }
         });
         ToDo.add(removeButton);
-
         startPanel.add(ToDo);
         startPanel.revalidate();
-
-
-        // Skapa och lagra en ny rad (ToDoItem)
-        //ToDoItem newItem = new ToDoItem(nrClicks, checkBox);
-        //toDoItems.add(newItem);
-
-        // Lägg till komponenterna i panelen
-        //toDoPanel.add(nrClicks);
-        //toDoPanel.add(checkBox);
-        //toDoPanel.add(removeButton);
-
-        // Lyssnare för "Ta bort"-knappen, tar bort raden från både GUI och listan
-        
     }
 
-    // Metod som tar bort en rad från både panelen och listan
    
+
     // Klass för att representera en rad med komponenter
     class ToDoItem {
         JTextField textField;
         JCheckBox checkBox;
-        
 
         ToDoItem(JTextField textField, JCheckBox checkBox) {
             this.textField = textField;
             this.checkBox = checkBox;
-            
         }
     }
+
+
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
